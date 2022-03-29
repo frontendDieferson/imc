54e3f098ca36af7229608ee7e5de27240efb0304
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-03-29 18:29:26 -0300

    Feature: Implementando Components e Lógica para Cálculo do IMC e exibindo no componente

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -15,6 +15,12 @@ export const GridItem = ({ item }: Props) => {
                <img src={item.icon === 'up' ? upImage : downImage} alt="" width="30" />
             </div>
             <div className={styles.gridTitle}>{item.title}</div>
+             
+             {item.yourImc &&
+                <div className={styles.yourImc}> Seu IMC é de: {item.yourImc} Kg/m² </div>
+
+             }
+
             <div className={styles.gridInfo}>
                 <>
                     O IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
