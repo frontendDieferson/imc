a068b5682a5b4be081c9185505ad4618050f72d8
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-03-29 18:08:50 -0300

    Feature Add: GridItem

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,4 +1,8 @@
 import { Level } from "../../helpers/imc"
+import styles from './GridItem.module.css';
+
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
 
 type Props = {
   item: Level
@@ -6,6 +10,17 @@ type Props = {
 
 export const GridItem = ({ item }: Props) => {
     return (
-        <div>GridItem</div>
+        <div className={styles.main} style={{ backgroundColor: item.color }}>
+            <div className={styles.gridIcon}>
+               <img src={item.icon === 'up' ? upImage : downImage} alt="" width="30" />
+            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+            <div className={styles.gridInfo}>
+                <>
+                    O IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
+
+        </div>
     )
 }
